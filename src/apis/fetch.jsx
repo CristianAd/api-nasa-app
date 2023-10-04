a7daa58fecb73d264438577c8cfa2ea3469e3481
CristianAd <sonlatin2@gmail.com>
2023-10-03 19:01:11 -0500

    Api funtionality

---
+++ src/apis/fetch.jsx
@@ -0,0 +1,11 @@
+const API_KEY = 'W6JHW76GGdZGdKvqcZbnzHDYFk0uFc9bUWBIeSHf';
+const API_URL = 'https://api.nasa.gov/planetary/apod?api_key=';
+
+
+export default asinc = ()=>{
+    try{
+        const response  = await fetch(`${API_URL}${API_KEY}`)
+    }catch(error);{
+
+    }
+}
